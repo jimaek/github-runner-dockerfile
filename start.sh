9dadd51d7b20fdbdb2c2aec52523c62077219886
Dmitriy Akulov <dakulovgr@gmail.com>
2024-04-16 18:46:25 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-ORG=$ORG
-ACCESS_TOKEN=$TOKEN
-
-echo "REPO ${REPOSITORY}"
+echo "ORG ${ORG}"
 echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
 REG_TOKEN=$(curl -X POST -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/orgs/${ORG}/actions/runners/registration-token | jq .token --raw-output)
+
+echo "REG_TOKEN ${REG_TOKEN}"
 
 cd /home/docker/actions-runner
 
