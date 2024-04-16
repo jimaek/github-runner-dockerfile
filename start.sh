2d264a8257769573b7e772e57199957393bbb0e2
Dmitriy Akulov <dakulovgr@gmail.com>
2024-04-16 19:40:50 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 echo "ORG ${ORG}"
 echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
-REG_TOKEN=$(curl -X POST -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/orgs/${ORG}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -X POST -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/orgs/${ORG}/actions/runners/registration-token  2>/dev/null | jq -r '.token')
 
 echo "REG_TOKEN ${REG_TOKEN}"
 
